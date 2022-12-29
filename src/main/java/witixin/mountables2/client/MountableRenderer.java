1e7edf55cedfb9b66d60bf52f0d63e0bbf22b738
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-12-29 20:57:09 +0100

    Fix the render layer

--- src/main/java/witixin/mountables2/client/MountableRenderer.java
+++ src/main/java/witixin/mountables2/client/MountableRenderer.java
@@ -1,9 +1,21 @@
 package witixin.mountables2.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.cache.texture.AutoGlowingTexture;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.GeoRenderer;
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
+import witixin.mountables2.Mountables2Mod;
 import witixin.mountables2.entity.Mountable;
+
+import javax.annotation.Nullable;
 
 public class MountableRenderer extends GeoEntityRenderer<Mountable> {
 
@@ -11,7 +23,33 @@ public class MountableRenderer extends GeoEntityRenderer<Mountable> {
 
     public MountableRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, MODEL);
-        this.addRenderLayer(new AutoGlowingGeoLayer<>(this));
+        this.addRenderLayer(new MountableRenderLayer(this));
     }
 
+
+    private static class MountableRenderLayer extends AutoGlowingGeoLayer<Mountable> {
+
+        public MountableRenderLayer(GeoRenderer<Mountable> renderer) {
+            super(renderer);
+        }
+
+        @Override
+        @Nullable
+        protected RenderType getRenderType(Mountable animatable) {
+            String location = animatable.getEmissiveTexture();
+            if (location.equals("transparent")) return null;
+            return AutoGlowingTexture.getRenderType(Mountables2Mod.rl("textures/" + location + ".png"));
+        }
+
+        @Override
+        public void render(PoseStack poseStack, Mountable animatable, BakedGeoModel bakedModel, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
+            RenderType type = getRenderType(animatable);
+
+            if (type != null) {
+                getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, type,
+                        bufferSource.getBuffer(type), partialTick, 15728640, OverlayTexture.NO_OVERLAY,
+                        1, 1, 1, 1);
+            }
+        }
+    }
 }
